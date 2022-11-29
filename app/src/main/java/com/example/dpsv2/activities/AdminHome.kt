d83b3c0e00f23ba6d097290772e2329fa513bb23
Bharat Jangama <bharatjb4@gmail.com>
2022-11-28 21:31:40 -0500

    added admin home file

---
+++ app/src/main/java/com/example/dpsv2/activities/AdminHome.kt
@@ -0,0 +1,12 @@
+package com.example.dpsv2.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dpsv2.R
+
+class AdminHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+    }
+}
